33f23e884e9ed8502aa8a6fe218612ffffd1a0b2
Nsikak-mbre <tm260715@gmail.com>
2025-01-31 16:58:10 +0100

    keep-alive

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify
+from flask import Flask, Response
+import orjson
 from datetime import datetime, timezone
 from flask_cors import CORS
 from flask_compress import Compress
@@ -7,6 +8,15 @@ from flask_compress import Compress
 app = Flask(__name__)
 CORS(app)
 Compress(app)
+app.config["COMPRESS_LEVEL"] = 9  # Max compression
+app.config["COMPRESS_MIN_SIZE"] = 512  # Min size to compress in bytes
+
+def orjson_response(data, status=200):
+    return Response(
+        orjson.dumps(data),
+        status=status,
+        mimetype="application/json"
+    )
 
 @app.route("/", methods=["GET"])
 def get_info():
@@ -15,7 +25,7 @@ def get_info():
         "current_datetime": datetime.now(timezone.utc).isoformat(),
         "github_url": "git@github.com:Nsikak-mbre/HNG12-stage-0.git"
     }
-    return jsonify(response)
+    return orjson_response(response)
 
 if __name__ == "__main__":
     app.run(debug=False, port=5000)
